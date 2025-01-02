18ca3aa989b9c4a1aedec815121356337b95d007
Ethan Li <ethanlimucheng@gmail.com>
2025-01-02 19:49:39 +0800

    Initial Commit

---
+++ SnakeAStar/SnakeAStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeAStarApp.swift
+//  SnakeAStar
+//
+//  Created by MuMu on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SnakeAStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
